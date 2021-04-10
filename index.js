965bf3f6f1de22c9c909664ff93d7e73fe8646ca
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-04-09 20:47:15 -0600

    linted

---
+++ index.js
@@ -0,0 +1,20 @@
+// index.js
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+fetchCoordsByIP('162.245.144.188', (error, data) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned coordinates", data);
+});
